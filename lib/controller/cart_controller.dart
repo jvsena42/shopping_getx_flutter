ad4cb6664af6a7e6e05e424d137539217ec82a07
jvsena42 <jvsena16@gmail.com>
2021-12-13 08:01:03 -0300

    feat: product_controller.dart and cart_controller.dart

---
+++ lib/controller/cart_controller.dart
@@ -0,0 +1,55 @@
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:shopping_getx_flutter/models/cart_item.dart';
+
+class CartController extends GetxController {
+  Map<int, CartItem> _items = {};
+
+  Map<int, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.productPrice * cartItem.productQuantity;
+    });
+    return total;
+  }
+
+  void addItem(int productId, double price, String title, int quantity) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              productTitle: existingCartItem.productTitle,
+              productQuantity: existingCartItem.productQuantity + 1,
+              productPrice: existingCartItem.productPrice));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          productTitle: title,
+          productPrice: price,
+          productQuantity: 1,
+        ),
+      );
+    }
+    update();
+  }
+
+  void removeItem(int productId) {
+    _items.remove(productId);
+    update();
+  }
+
+  void clear() {
+    _items = {};
+    update();
+  }
+}
