07b4dcd35b33e4e898f3f7fd5aa374102cba78bf
jvsena42 <jvsena16@gmail.com>
2021-12-14 08:17:10 -0300

    feat: app_drawer.dart

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopping_getx_flutter/controller/order_controller.dart';
+import 'package:shopping_getx_flutter/widget/app_drawer.dart';
+import 'package:shopping_getx_flutter/widget/order_item.dart';
 
 class OrderScreen extends StatelessWidget {
   var orderController = Get.put(OrderController());
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopping_getx_flutter/controller/cart_controller.dart';
+import 'package:shopping_getx_flutter/widget/app_drawer.dart';
 import 'package:shopping_getx_flutter/widget/product_grid.dart';
 
 import 'cart_screen.dart';
