a4c63c1dda63a099ff42bcd35391e21ddff425b0
jvsena42 <jvsena16@gmail.com>
2021-12-14 07:20:08 -0300

    feat: product_grid.dart

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopping_getx_flutter/controller/cart_controller.dart';
+import 'package:shopping_getx_flutter/widget/product_grid.dart';
 
 class ProductOverviewPage extends StatelessWidget {
   @override
